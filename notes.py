36423bacf3356719ff057d1e1dc2ea4782e90d6b
Moatasem Baka (MB9) <assombaka500@gmail.com>
2024-02-23 22:06:16 +0200

    modified:   notes.py

--- notes.py
+++ notes.py
@@ -1,58 +1,82 @@
+# Displaying a note
+# This is a note explaining the purpose of this code section
 print("hello this is notes file for python commands that i'll show throughout the code")
+
+# Integer Command
+# Creating an integer and displaying it
+x = int(3)
 print("that is an int command")
-print(int(3))
+print(x)
 
+# Float Command
+# Creating a float and displaying it
+y = float(3)
 print("that is a float command")
+print(y)
 
-print(float(3))
-
+# Boolean Command
+# Creating a boolean value and displaying it
 print("that is a boolean command")
-
 print(True)
 
+# String Command
+# Creating a string and displaying it
 print("that is a string command")
-
 print("this is a string")
 
+# List Command
+# Creating a list and displaying it
 print("that is a list command")
-
 print([1,2,3])
 
+# Dictionary Command
+# Creating a dictionary and displaying it
 print("that is a dictionary command")
-
 print({1:2,3:4})
 
+# Tuple Command
+# Creating a tuple and displaying it
 print("that is a tuple command")
-
 print((1,2,3))
 
+# Set Command
+# Creating a set and displaying it
 print("that is a set command")
-
 print({1,2,3})
 
+# Range Command
+# Creating a range object and displaying it
 print("that is a range command")
-
 print(range(10))
 
+# Enumerate Command
+# Enumerating over a list and displaying the enumerate object
 print("that is a enumerate command")
-
 print(enumerate([1,2,3]))
 
+# Zip Command
+# Zipping two lists and displaying the zip object
 print("that is a zip command")
-
 print(zip([1,2,3],[4,5,6]))
 
+# Conditional Statements
+# Checking the value of x and printing a message accordingly
+x = 1
+if x == 1:
+    print("this is an if command")
+elif x == 2:
+    print("this is an elif command")
+else:
+    print("this is an else command")
 
-if True:
-    print("this is a if command")
-
+# For Loop
+# Iterating over a list and printing each element
 print("that is a for command")
-
 for i in [1,2,3]:
     print(i)
 
+# While Loop
+# Printing a message indefinitely
+print("that is a while command")
 while True:
     print("this is a while command")
-
-
-
