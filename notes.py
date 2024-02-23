d64149ccf9d3b26723fe0f53b906a64d91800da3
Moatasem Baka (MB9) <assombaka500@gmail.com>
2024-02-23 21:58:47 +0200

    modified:   notes.py

--- notes.py
+++ notes.py
@@ -1,1 +1,58 @@
 print("hello this is notes file for python commands that i'll show throughout the code")
+print("that is an int command")
+print(int(3))
+
+print("that is a float command")
+
+print(float(3))
+
+print("that is a boolean command")
+
+print(True)
+
+print("that is a string command")
+
+print("this is a string")
+
+print("that is a list command")
+
+print([1,2,3])
+
+print("that is a dictionary command")
+
+print({1:2,3:4})
+
+print("that is a tuple command")
+
+print((1,2,3))
+
+print("that is a set command")
+
+print({1,2,3})
+
+print("that is a range command")
+
+print(range(10))
+
+print("that is a enumerate command")
+
+print(enumerate([1,2,3]))
+
+print("that is a zip command")
+
+print(zip([1,2,3],[4,5,6]))
+
+
+if True:
+    print("this is a if command")
+
+print("that is a for command")
+
+for i in [1,2,3]:
+    print(i)
+
+while True:
+    print("this is a while command")
+
+
+
